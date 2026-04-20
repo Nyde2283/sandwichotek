47f419818fcadc71dac9046425b5baf13dd6fda8
Nyde <edwynchev@gmail.com>
2026-04-20 15:04:51 +0200

    refactor: use constant HTTP codes in misc tests

--- tests/test_miscs.py
+++ tests/test_miscs.py
@@ -1,14 +1,16 @@
+from fastapi import status
+
 def test_ping(client):
     response = client.get("/ping")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == "pong"
 
 def test_schema_diagram(client):
     response = client.get("/schema_diagram")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.headers["content-type"] == "image/svg+xml"
 
 def test_get_favicon(client):
     response = client.get("/favicon.ico")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.headers["content-type"] == "image/png"
