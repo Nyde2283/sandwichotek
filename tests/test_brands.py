90ec3898654e32c8bf2db08bc636fbbcfbc3807c
Nyde <edwynchev@gmail.com>
2026-04-20 14:58:00 +0200

    test: rework and complete tests for shelfs and brands

--- tests/test_brands.py
+++ tests/test_brands.py
@@ -1,3 +1,4 @@
+from fastapi import status
 from backend.db.models import Brand
 
 # ---------------------------------------------------------------------------- #
@@ -8,10 +9,10 @@ def test_create_brand(client, session):
     body = {"name": "pouce"}
     response = client.post("/brands", json=body)
 
-    assert response.status_code == 200
-    data = response.json()
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
         **body,
+        "ingredients": [],
         "id": 1
     }
     db_brand = session.get(Brand, 1)
@@ -27,29 +28,32 @@ def test_create_brand(client, session):
 
 def test_get_brand_list_on_empty_db(client):
     response = client.get("/brands")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == []
 
 def test_get_brand_list(client, session, seeded_db):
     response = client.get("/brands")
-    assert response.status_code == 200
-    assert response.json() == [{
-        **brand,
-        "id": i + 1
-    } for i, brand in enumerate(seeded_db[Brand])]
+    assert response.status_code == status.HTTP_200_OK
+
+    for i, brand in enumerate(seeded_db[Brand]):
+        assert {
+            **brand,
+            "id": i + 1
+        }.items() <= response.json()[i].items()
+        assert response.json()[i]["ingredients"] != []
 
 def test_get_brand_by_id_on_empty_db(client):
     response = client.get("/brands/1")
-    assert response.status_code == 404
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
-def test_get_brand_by_id(client, session, seeded_db):
-    for i in range(len(seeded_db[Brand])):
+def test_get_brand_by_id(client, seeded_db):
+    for i, brand in enumerate(seeded_db[Brand]):
         response = client.get(f"/brands/{i + 1}")
-        assert response.status_code == 200
-        assert response.json() == {
-            **seeded_db[Brand][i],
+        assert response.status_code == status.HTTP_200_OK
+        assert {
+            **brand,
             "id": i + 1
-        }
+        }.items() <= response.json().items()
 
 # ---------------------------------------------------------------------------- #
 #                                      PUT                                     #
@@ -59,23 +63,20 @@ def test_update_brand_by_id_on_empty_db(client):
     body = {"name": "pouce"}
     response = client.put("/brands/1", json=body)
 
-    assert response.status_code == 404
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
 def test_update_brand_by_id(client, session, seeded_db):
     body = {"name": "p0uc3"}
     response = client.put("/brands/2", json=body)
 
-    assert response.status_code == 200
-    assert response.json() == {
+    assert response.status_code == status.HTTP_200_OK
+    assert {
         **body,
         "id": 2
-    }
+    }.items() <= response.json().items()
     db_brand = session.get(Brand, 2)
     assert db_brand != None
-    assert db_brand.model_dump() == {
-        **body,
-        "id": 2
-    }
+    assert db_brand.name == body["name"]
 
 # ---------------------------------------------------------------------------- #
 #                                    DELETE                                    #
@@ -84,10 +85,24 @@ def test_update_brand_by_id(client, session, seeded_db):
 def test_delete_brand_by_id_on_empty_db(client, session):
     assert session.get(Brand, 1) == None
     response = client.delete("/brands/1")
-    assert response.status_code == 404
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
-def test_delete_brand_by_id(client, session, seeded_db):
+def test_delete_brand_by_id_conflict_foreign_key(client, session, seeded_db):
     response = client.delete("/brands/2")
 
-    assert response.status_code == 200
-    assert session.get(Brand, 2) == None
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+    assert response.json()["detail"]["msg"] == "Foreign key violation ! Check ingredients field"
+    assert response.json()["detail"].get("original error") != None
+    assert session.get(Brand, 2) != None
+
+def test_delete_brand_by_id(client, session, seeded_db):
+    # Create new object which is not referenced
+    brand = Brand(name="to_delete")
+    session.add(brand)
+    session.commit()
+    brand_id = brand.id
+
+    response = client.delete(f"/brands/{brand.id}")
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert session.get(Brand, brand_id) == None
--- tests/test_shelfs.py
+++ tests/test_shelfs.py
@@ -1,3 +1,4 @@
+from fastapi import status
 from backend.db.models import Shelf
 
 # ---------------------------------------------------------------------------- #
@@ -8,10 +9,10 @@ def test_create_shelf(client, session):
     body = {"name": "fromages"}
     response = client.post("/shelfs", json=body)
 
-    assert response.status_code == 200
-    data = response.json()
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
         **body,
+        "ingredients": [],
         "id": 1
     }
     db_shelf = session.get(Shelf, 1)
@@ -27,29 +28,32 @@ def test_create_shelf(client, session):
 
 def test_get_shelf_list_on_empty_db(client):
     response = client.get("/shelfs")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == []
 
-def test_get_shelf_list(client, session, seeded_db):
+def test_get_shelf_list(client, seeded_db):
     response = client.get("/shelfs")
-    assert response.status_code == 200
-    assert response.json() == [{
-        **shelf,
-        "id": i + 1
-    } for i, shelf in enumerate(seeded_db[Shelf])]
+    assert response.status_code == status.HTTP_200_OK
+
+    for i, shelf in enumerate(seeded_db[Shelf]):
+        assert {
+            **shelf,
+            "id": i + 1
+        }.items() <= response.json()[i].items()
+        assert response.json()[i]["ingredients"] != []
 
 def test_get_shelf_by_id_on_empty_db(client):
     response = client.get("/shelfs/1")
-    assert response.status_code == 404
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
-def test_get_shelf_by_id(client, session, seeded_db):
-    for i in range(len(seeded_db[Shelf])):
+def test_get_shelf_by_id(client, seeded_db):
+    for i, shelf in enumerate(seeded_db[Shelf]):
         response = client.get(f"/shelfs/{i + 1}")
-        assert response.status_code == 200
-        assert response.json() == {
-            **seeded_db[Shelf][i],
+        assert response.status_code == status.HTTP_200_OK
+        assert {
+            **shelf,
             "id": i + 1
-        }
+        }.items() <= response.json().items()
 
 # ---------------------------------------------------------------------------- #
 #                                      PUT                                     #
@@ -59,23 +63,20 @@ def test_update_shelf_by_id_on_empty_db(client):
     body = {"name": "fromages"}
     response = client.put("/shelfs/1", json=body)
 
-    assert response.status_code == 404
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
 def test_update_shelf_by_id(client, session, seeded_db):
     body = {"name": "fr0m@g3"}
     response = client.put("/shelfs/3", json=body)
 
-    assert response.status_code == 200
-    assert response.json() == {
+    assert response.status_code == status.HTTP_200_OK
+    assert {
         **body,
         "id": 3
-    }
+    }.items() <= response.json().items()
     db_shelf = session.get(Shelf, 3)
     assert db_shelf != None
-    assert db_shelf.model_dump() == {
-        **body,
-        "id": 3
-    }
+    assert db_shelf.name == body["name"]
 
 # ---------------------------------------------------------------------------- #
 #                                    DELETE                                    #
@@ -84,10 +85,24 @@ def test_update_shelf_by_id(client, session, seeded_db):
 def test_delete_shelf_by_id_on_empty_db(client, session):
     assert session.get(Shelf, 1) == None
     response = client.delete("/shelfs/1")
-    assert response.status_code == 404
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
-def test_delete_shelf_by_id(client, session, seeded_db):
+def test_delete_shelf_by_id_conflict_foreign_key(client, session, seeded_db):
     response = client.delete("/shelfs/3")
 
-    assert response.status_code == 200
-    assert session.get(Shelf, 3) == None
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+    assert response.json()["detail"]["msg"] == "Foreign key violation ! Check ingredients field"
+    assert response.json()["detail"].get("original error") != None
+    assert session.get(Shelf, 3) != None
+
+def test_delete_shelf_by_id(client, session, seeded_db):
+    # Create new object which is not referenced
+    shelf = Shelf(name="to_delete")
+    session.add(shelf)
+    session.commit()
+    shelf_id = shelf.id
+
+    response = client.delete(f"/shelfs/{shelf.id}")
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert session.get(Shelf, shelf_id) == None
