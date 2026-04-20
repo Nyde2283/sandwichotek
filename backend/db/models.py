81f66fa0d53cb6c4f46175ccc93c6da505af5320
Nyde <edwynchev@gmail.com>
2026-04-20 14:43:06 +0200

    fix: enforce RESTRICT behavior instead of SET NULL default
    Prevent SET NULL behavior when deleting an object referenced by a foreign key in another object.

--- backend/db/models.py
+++ backend/db/models.py
@@ -16,7 +16,7 @@ class ShelfBase(SQLModel):
 class Shelf(ShelfBase, table=True):
     id: int | None = Field(default=None, primary_key=True)   # see https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/#primary-key-id
 
-    ingredients: list[Ingredient] = Relationship(back_populates="shelf")   # see https://sqlmodel.tiangolo.com/tutorial/relationship-attributes/back-populates/#relationship-with-back_populates
+    ingredients: list[Ingredient] = Relationship(back_populates="shelf", passive_deletes="all")   # see https://sqlmodel.tiangolo.com/tutorial/relationship-attributes/back-populates/#relationship-with-back_populates
 db_tables.append(Shelf)
 
 class ShelfCreate(ShelfBase):
@@ -36,7 +36,7 @@ class BrandBase(SQLModel):
 class Brand(BrandBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
 
-    ingredients: list[Ingredient] = Relationship(back_populates="brand")
+    ingredients: list[Ingredient] = Relationship(back_populates="brand", passive_deletes="all")
 db_tables.append(Brand)
 
 class BrandCreate(BrandBase):
@@ -57,8 +57,8 @@ class MealBase(SQLModel):
 class Meal(MealBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
 
-    meal_productions: list[MealProduction] = Relationship(back_populates="meal")
-    recipe_items: list[RecipeItem] = Relationship(back_populates="meal")
+    meal_productions: list[MealProduction] = Relationship(back_populates="meal", passive_deletes="all")
+    recipe_items: list[RecipeItem] = Relationship(back_populates="meal", passive_deletes="all")
 db_tables.append(Meal)
 
 class MealCreate(MealBase):
@@ -77,15 +77,15 @@ class IngredientBase(SQLModel):
     name: str = Field(index=True)
     unit: str
     remark: str | None = Field(default=None)
-    shelf_id: int | None = Field(default=None, foreign_key="shelf.id", ondelete="SET NULL")
-    brand_id: int | None = Field(default=None, foreign_key="brand.id", ondelete="SET NULL")
+    shelf_id: int | None = Field(default=None, foreign_key="shelf.id", ondelete="RESTRICT")
+    brand_id: int | None = Field(default=None, foreign_key="brand.id", ondelete="RESTRICT")
 
 class Ingredient(IngredientBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
 
     shelf: Shelf | None = Relationship(back_populates="ingredients")
     brand: Brand | None = Relationship(back_populates="ingredients")
-    recipe_items: list[RecipeItem] = Relationship(back_populates="ingredient")
+    recipe_items: list[RecipeItem] = Relationship(back_populates="ingredient", passive_deletes="all")
 db_tables.append(Ingredient)
 
 class IngredientCreate(IngredientBase):
@@ -109,50 +109,48 @@ class IngredientUpdate(SQLModel):
 # ---------------------------------------------------------------------------- #
 
 class RecipeItemBase(SQLModel):
+    meal_id: int = Field(default=None, primary_key=True, foreign_key="meal.id", ondelete="RESTRICT")
+    ingredient_id: int = Field(default=None, primary_key=True, foreign_key="ingredient.id", ondelete="RESTRICT")
     quantity: float
 
 class RecipeItem(RecipeItemBase, table=True):
-    meal_id: int | None = Field(default=None, primary_key=True, foreign_key="meal.id", ondelete="RESTRICT")
-    ingredient_id: int | None = Field(default=None, primary_key=True, foreign_key="ingredient.id", ondelete="RESTRICT")
-
     meal: Meal = Relationship(back_populates="recipe_items")
     ingredient: Ingredient = Relationship(back_populates="recipe_items")
 db_tables.append(RecipeItem)
 
 class RecipeItemCreate(RecipeItemBase):
-    meal_id: int
-    ingredient_id: int
+    pass
 
 class RecipeItemPublic(RecipeItemBase):
-    meal_id: int
-    ingredient_id: int
+    pass
 
 class RecipeItemPublicVerbose(RecipeItemPublic):
     meal: Meal
     ingredient: Ingredient
 
 class RecipeItemUpdate(SQLModel):
+    meal_id: int | None = None
+    ingredient_id: int | None = None
     quantity: int | None = None
 
 # ---------------------------------------------------------------------------- #
 
 class MealProductionBase(SQLModel):
+    meal_id: int = Field(foreign_key="meal.id", nullable=False, ondelete="RESTRICT")
     date: date
     quantity: int
 
 class MealProduction(MealProductionBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
 
-    meal_id: int = Field(foreign_key="meal.id", ondelete="RESTRICT")
     meal: Meal = Relationship(back_populates="meal_productions")
 db_tables.append(MealProduction)
 
 class MealProductionCreate(MealProductionBase):
-    meal_id: int
+    pass
 
 class MealProductionPublic(MealProductionBase):
     id: int
-    meal_id: int
 
 class MealProductionVerbose(MealProductionPublic):
     meal: Meal
